b285a614b3151e168d728f3e5dd0e11a36db471c
meet <meet@meet>
2014-08-11 18:12:33 +0300

    login page

---
+++ main-hanna.py
@@ -0,0 +1,28 @@
+import kivy
+kivy.require('1.8.0')
+
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.image import Image
+from kivy.graphics import *
+
+sm=ScreenManager()
+
+class Backgruond(Image):
+    pass
+
+class LogIn(Screen):
+    pass
+
+
+class LogInApp(App):
+    def build(self):
+        self.LogIn=LogIn(name="LogIn")
+        sm.add_widget(self.LogIn)
+        return sm
+        
+        
+
+        
+if __name__=="__main__":
+    LogInApp().run()
